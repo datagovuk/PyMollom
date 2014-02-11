3d0ad99b6242639dcc0914d2ddbd1fd1017a82e2
Ross Jones <ross@servercode.co.uk>
2014-02-11 09:52:47 +0000

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
-"""Setup script for PyMollom"""
+"""Setup script for mollom"""
 from distutils.core import setup
 
-setup(name='PyMollom',
+setup(name='mollom',
       version='0.1',
       license='GPL',
       namespace_packages=['mollom'],
       zip_safe=False,
       packages=find_packages(exclude=['ez_setup']),
       include_package_data=False,
-      description='A Python library for the Mollom anti-spam service',
+      description='A Python library for the Mollom anti-spam service using the original PyMollom code',
       long_description=open('README.md').read(),
       classifiers=[
           'Development Status :: 5 - Production/Stable',
